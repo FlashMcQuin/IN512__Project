f6deb49bcc8a8805d5bd4052611509142ee53097
lisamignerey83 <lisa_832601@hotmail.fr>
2023-11-22 15:09:05 +0100

    je suis la meilleure

--- scripts/agent.py
+++ scripts/agent.py
@@ -10,7 +10,7 @@ from my_constants import *
 from threading import Thread
 import numpy as np
 #thuis is the agent 
-
+#hello I'm the queen 
 class Agent:
     """ Class that implements the behaviour of each agent based on their perception and communication with other agents """
     def __init__(self, server_ip):
